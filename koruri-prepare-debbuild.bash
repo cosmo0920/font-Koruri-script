c5c8ecdceea626b3ded40c9d9d7fe6b897a9c10a
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-07-23 22:18:19 +0900

    modify packaging script

--- koruri-prepare-debbuild.bash
+++ koruri-prepare-debbuild.bash
@@ -5,6 +5,10 @@ wget https://github.com/Koruri/Koruri/archive/Koruri-${KORURIVER}.tar.gz
 tar xzvf Koruri-${KORURIVER}.tar.gz
 
 cd Koruri-Koruri-${KORURIVER}
+# Github version README.md witten in English
+mv README.md README_E.md
+# add extension `.txt`
+mv LICENSE LICENSE.txt
 cp ../mplus-LICENSE/* .
 rm -rf sfd
 cd -
