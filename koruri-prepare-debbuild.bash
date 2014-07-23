4e06d2461152605fe6a7795c9b1a3a8c5e7e5219
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-07-23 10:03:14 +0900

    wget from Github `Koruri`

--- koruri-prepare-debbuild.bash
+++ koruri-prepare-debbuild.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 KORURIVER=20140524
-tar xvfJ Koruri-${KORURIVER}.tar.xz
+wget https://github.com/Koruri/Koruri/archive/Koruri-${KORURIVER}.tar.gz
+tar xzvf Koruri-${KORURIVER}.tar.gz
 
-cd Koruri-${KORURIVER}
+cd Koruri-Koruri-${KORURIVER}
 cp ../mplus-LICENSE/* .
 rm -rf sfd
 cd -
-tar Jcfv fonts-koruri_${KORURIVER}.orig.tar.xz Koruri-${KORURIVER}
+tar Jcfv fonts-koruri_${KORURIVER}.orig.tar.xz Koruri-Koruri-${KORURIVER}
 DESTDIR=..
 cp fonts-koruri_${KORURIVER}.orig.tar.xz ${DESTDIR}
